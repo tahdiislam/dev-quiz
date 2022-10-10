5722dcc110cff1ba086d30b2967a386c2a293233
tahdi-islam <tshdi60@gmail.com>
2022-10-11 03:14:12 +0600

    add active link in navbar

--- src/Component/Header/HeaderItems.jsx
+++ src/Component/Header/HeaderItems.jsx
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 const HeaderItems = ({route}) => {
     const {name, path} = route;
     return (
-        <li className='md:mr-8 w-full py-2 my-1 bg-indigo-600 rounded text-white hover:text-gray-300'><NavLink className='w-full cursor-pointer' to={path}>{name}</NavLink></li>
+        <li className='md:mr-8 w-full py-2 my-1 bg-indigo-600 rounded text-white hover:text-gray-300'><NavLink className={({ isActive }) =>
+              isActive ? 'py-1 border-0 border-b-2' : undefined} to={path}>{name}</NavLink></li>
     );
 };
 
