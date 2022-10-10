3da6d52595a9fdc5671808d4034a90eaa100d30e
tahdi-islam <tshdi60@gmail.com>
2022-10-11 05:52:09 +0600

    show the questions with radio btn

--- src/Component/Quiz/Quiz.jsx
+++ src/Component/Quiz/Quiz.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const Quiz = ({quiz}) => {
-    const {options, question: initialQuestons, correctAnswer} = quiz;
+    const {options, question: initialQuestons, correctAnswer, id} = quiz;
     const questionModify01 = initialQuestons.split('<p>');
     const questionModify02 = questionModify01.join(' ');
     const questionModify03 = questionModify02.split('</p>')
     const question = questionModify03.join(' ')
     console.log(question);
     return (
-        <div>
-              <h2>{question}</h2>
+        <div className='border'>
+              <h2 className='text-3xl font-medium'>{question}</h2>
+              <div >
+                {
+                    options.map(option => <QuizOption option={option} id={id} key={id}/>)
+                }
+              </div>
         </div>
     );
 };
+
+const QuizOption = ({option, id}) =>{
+    return(
+        <label className='text-2xl'><input className='h-6 w-6' type="radio" name={id} id={id} value={option}/>{option}</label>
+    )
+}
 
 export default Quiz;
